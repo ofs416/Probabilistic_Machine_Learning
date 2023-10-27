cf4139825f09d3e9aefe0b6a5a0f6ed83aa8ccc7
ofs416 <os416@cam.ac.uk>
2023-10-27 16:55:02 +0100

    task A

---
+++ Coursework1/A.m
@@ -0,0 +1,23 @@
+clear all;                              % Clear and close all previous 
+close all;                              % variables and figures
+
+load cw1a.mat;                          % Load in the data for training
+xs = linspace(-4, 4, 61)';              % 61 test inputs
+
+meanfunc = []; hyp.mean = [];           % empty: don't use a mean function
+covfunc = @covSEiso; hyp.cov = [-1 0];  % Squared Exponental covariance function
+likfunc = @likGauss; hyp.lik = 0;       % Gaussian likelihood   
+
+hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, ...
+                likfunc, x, y);
+
+disp(' ')
+nlml = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, x, y)
+disp(' ')
+
+[mu s2] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
+
+
+f = [mu+2*sqrt(s2); flipdim(mu-2*sqrt(s2), 1)];
+     fill([xs; flipdim(xs,1)], f, [7 7 7]/8)
+     hold on; plot(xs, mu); plot(x, y, '+');
